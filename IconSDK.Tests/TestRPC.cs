a2502c0d6416a109d422c5c026fb8f540db47683
spl3en <spl3en.contact@gmail.com>
2018-11-30 01:49:12 +0100

    Improve GetBalance unit test.

--- IconSDK.Tests/TestRPC.cs
+++ IconSDK.Tests/TestRPC.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 namespace IconSDK.Tests
 {
     using RPC;
+
     public class TestRPC
     {
         private const string API_URL = "https://testwallet.icon.foundation/api/v3";
@@ -13,8 +14,22 @@ namespace IconSDK.Tests
         public async Task Test_GetBalance()
         {
             var GetBalance = new GetBalance(API_URL);
+
+            // case 0: Successfull calls
+            // Get balance from EOA or score successfully
             var balance = await GetBalance.Invoke("hx0000000000000000000000000000000000000000");
-            Console.WriteLine($"Treasury : {balance.ToString()}");
+            Assert.IsInstanceOf<System.Numerics.BigInteger>(balance);
+            // No prefix
+            var balance2 = await GetBalance.Invoke("0000000000000000000000000000000000000000");
+            Assert.True(balance == balance2);
+
+            // case 1: when a param is wrong.
+            // Wrong hex format
+            Assert.ThrowsAsync<FormatException>(() => GetBalance.Invoke("hxWRONG"));
+            // Wrong prefix
+            Assert.ThrowsAsync<FormatException>(() => GetBalance.Invoke("0x0000000000000000000000000000000000000000"));
+            // Wrong number of bytes
+            Assert.ThrowsAsync<Exception>(() => GetBalance.Invoke("hx0000000000000000000"));
         }
     }
 }
