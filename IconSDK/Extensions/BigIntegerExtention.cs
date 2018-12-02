83f9fa681597164cb4adbaa395521542ef1a64f6
Daehee Kim <zsaladinz@gmail.com>
2018-12-03 00:21:18 +0900

    Fix BigIntegerExtension to parse '0x' hex string properly.

--- IconSDK/Extensions/BigIntegerExtention.cs
+++ IconSDK/Extensions/BigIntegerExtention.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Numerics;
+using System.Globalization;
+using Newtonsoft.Json;
 
 namespace IconSDK.Extensions
 {
@@ -6,12 +9,56 @@ namespace IconSDK.Extensions
     {
         public static string ToHex(this BigInteger bigInteger)
         {
-            return bigInteger.ToString("x");
+            if (bigInteger >= 0)
+            {
+                var result = bigInteger.ToString("x");
+                if (result.Length > 1 && result[0] == '0')
+                    result = result.Substring(1);
+                return result;
+            }
+            else
+            {
+                bigInteger = -bigInteger;
+                return $"-{bigInteger.ToString("x")}";
+            }
         }
 
         public static string ToHex0x(this BigInteger bigInteger)
         {
-            return $"0x{bigInteger.ToHex()}";
+            if (bigInteger >= 0)
+            {
+                return $"0x{bigInteger.ToHex()}";
+            }
+            else
+            {
+                bigInteger = -bigInteger;
+                return $"-0x{bigInteger.ToString("x")}";
+            }
+        }
+
+        public static BigInteger ToBigInteger(this string hex)
+        {
+            hex = hex.Replace("0x", "00");
+            bool isNegative = (hex[0] == '-');
+            if (isNegative)
+                hex = hex.Replace("-", string.Empty);
+
+            var result = BigInteger.Parse(hex, NumberStyles.AllowHexSpecifier);
+            return isNegative ? -result : result;
+        }
+    }
+
+    public class BigIntegerConverter : JsonConverter<BigInteger>
+    {
+        public override BigInteger ReadJson(JsonReader reader, Type objectType, BigInteger existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            string s = (string)reader.Value;
+            return s.ToBigInteger();
+        }
+
+        public override void WriteJson(JsonWriter writer, BigInteger value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToHex0x());
         }
     }
 }
